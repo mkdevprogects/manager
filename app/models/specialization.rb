735a39381857da7e6149afa2a16a6f7007f30c8f
Anton <anton@ondoc.me>
2017-08-15 00:19:08 +0300

    Add join models to Doctor, Specialzation, Disease, Symptom

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -1,3 +1,9 @@
 class Specialization < ActiveRecord::Base
+  has_many :doctor_specializations
+  has_many :doctors, through: :doctor_specializations
+
+  has_many :disease_specializations
+  has_many :diseases, through: :disease_specializations
+
   validates :title, :code, presence: true
 end
--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,3 +1,6 @@
 class Symptom < ActiveRecord::Base
+  has_many :disease_symptoms
+  has_many :diseases, through: :disease_symptoms
+
   validates :title, presence: true
 end
