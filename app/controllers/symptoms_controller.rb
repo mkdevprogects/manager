f4eade705507dd74f546b6cb62bb49d1ced2fcd2
Anton <anton@ondoc.me>
2017-08-16 22:32:47 +0300

    Create Symptoms#index view and controller

---
+++ app/controllers/symptoms_controller.rb
@@ -0,0 +1,23 @@
+class SymptomsController < ApplicationController
+  def index
+    @symptoms = Symptom.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :symptoms
+
   resources :home, only: [:index]
 
   devise_for :admin
