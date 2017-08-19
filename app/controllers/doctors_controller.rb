b617002de0b40d4f51c6a6e79f039d3f3ed77c8d
Anton <anton@ondoc.me>
2017-08-19 23:46:46 +0300

    Create Doctor CRUD

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,58 @@
+class DoctorsController < BaseController
+  before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @doctors = Doctor.all.decorate
+  end
+
+  def show
+  end
+
+  def new
+    @doctor = Doctor.new.decorate
+  end
+
+  def edit
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params).decorate
+    if @doctor.save
+      redirect_to @doctor, notice: 'Врач успешно создан.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @doctor.update(doctor_params)
+      redirect_to @doctor, notice: 'Врач успешно изменен.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @doctor.destroy
+    redirect_to doctors_url, notice: 'Врач успешно удален.'
+  end
+
+  private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id]).decorate
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(
+        :name,
+        :surname,
+        :patronymic,
+        :phone,
+        :email,
+        clinic_ids: [],
+        specialization_ids: [],
+        grade_ids: []
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :symptoms
   resources :diseases
 
+  resources :doctors
+
   resources :home, only: [:index]
 
   devise_for :admin
