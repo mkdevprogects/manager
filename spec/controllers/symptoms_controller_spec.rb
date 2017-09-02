52b09371943c872685c6d5ab4cce103f04c8207e
Anton <anton@ondoc.me>
2017-09-02 12:10:48 +0300

    Update tests SymptomsConroller

--- spec/controllers/symptoms_controller_spec.rb
+++ spec/controllers/symptoms_controller_spec.rb
@@ -9,139 +9,133 @@ RSpec.describe SymptomsController, type: :controller do
     let(:admin) { create(:admin) }
 
     before { sign_in admin }
-    describe "GET #index" do
+    describe 'GET #index' do
       let!(:symptom_1) { create(:symptom) }
       let!(:symptom_2) { create(:symptom) }
 
       before { get :index }
 
-      it "returns http success" do
+      it 'returns http success' do
         expect(response).to have_http_status(:success)
       end
 
-      it "renders the index template" do
-        expect(response).to render_template("index")
+      it 'renders the index template' do
+        expect(response).to render_template(:index)
       end
 
-      it "loads all symptoms" do
+      it 'loads all symptoms' do
         expect(assigns(:symptoms)).to match_array([symptom_1, symptom_2])
-      end
-
-      it "page have symptoms title" do
-        expect(response.body).to include("#{symptom_1.title}","#{symptom_2.title}")
       end
     end
 
-    describe "GET #show" do
+    describe 'GET #show' do
       let(:symptom) { create(:symptom, diseases: [create(:disease)]) }
 
       before { get :show, { id: symptom.id} }
 
-      it "returns http success" do
+      it 'returns http success' do
         expect(response).to have_http_status(:success)
       end
 
-      it "renders the show template" do
-        expect(response).to render_template("show")
+      it 'renders the show template' do
+        expect(response).to render_template(:show)
       end
 
-      it "load symptom" do
+      it 'load symptom' do
         expect(assigns(:symptom)).to eq(symptom)
       end
-
-      it "page have symptoms title" do
-        expect(response.body).to include("#{symptom.title}")
-      end
-
-      it "page have symptoms description" do
-        expect(response.body).to include("#{symptom.description}")
-      end
-
-      it "page have symptoms disease" do
-        expect(response.body).to include("#{symptom.diseases.first.title}")
-      end
     end
 
-    describe "GET #new" do
+    describe 'GET #new' do
       before { get :new }
 
-      it "returns http success" do
+      it 'returns http success' do
         expect(response).to have_http_status(:success)
       end
 
-      it "renders the new template" do
-        expect(response).to render_template("new")
+      it 'renders the new template' do
+        expect(response).to render_template(:new)
       end
     end
 
-    describe "GET #edit" do
+    describe 'GET #edit' do
       before { get :edit, { id: symptom.id} }
 
-      it "returns http success" do
+      it 'returns http success' do
         expect(response).to have_http_status(:success)
       end
 
-      it "renders the edit template" do
-        expect(response).to render_template("edit")
-      end
-
-      it "page have symptoms title" do
-        expect(response.body).to include("#{symptom.title}")
-      end
-
-      it "page have symptoms description" do
-        expect(response.body).to include("#{symptom.description}")
+      it 'renders the edit template' do
+        expect(response).to render_template(:edit)
       end
     end
 
-    describe "POST #create" do
-      it "returns http 302" do
-        post :create, { symptom: { title: symptom.title }}
-        expect(response).to have_http_status(302)
+    describe 'POST #create' do
+      context 'valid data' do
+        it 'redirect to show' do
+          post :create, { symptom: attributes_for(:symptom)}
+          expect(response).to redirect_to assigns(:symptom)
+        end
+      end
+
+      context 'not valid data' do
+        it 'renders the new template' do
+          post :create, { symptom: { title: nil }}
+          expect(response).to render_template(:new)
+        end
       end
     end
 
-    describe "POST #update" do
-      it "returns http 302" do
-        patch :update, { id: symptom.id, symptom: { title: symptom.title }}
-        expect(response).to have_http_status(302)
+    describe 'POST #update' do
+      context 'valid data' do
+        it 'redirect to show' do
+          patch :update, { id: symptom.id, symptom: attributes_for(:symptom) }
+          expect(response).to redirect_to assigns(:symptom)
+        end
+      end
+
+      context 'not valid data' do
+        it 'renders the edit template' do
+          patch :update, { id: symptom.id, symptom: { title: nil }}
+          expect(response).to render_template(:edit)
+        end
       end
     end
 
-    describe "POST #destroy" do
-      it "returns http 302" do
+    describe 'POST #destroy' do
+      it 'redirect to index' do
         delete :destroy, { id: symptom.id}
-        expect(response).to have_http_status(302)
+        expect(response).to redirect_to action: :index
       end
     end
   end
 
   context 'guest' do
-    describe "GET #index" do
-      it "returns http 302" do
+    describe 'GET #index' do
+      it 'redirect to admin/sign_in' do
         get :index
-        expect(response).to have_http_status(302)
+        expect(response).to admin_sign_in
       end
     end
 
-    describe "GET #show" do
-      it "returns http 302" do
+    describe 'GET #show' do
+      it 'redirect to admin/sign_in' do
         get :show, { id: symptom.id}
-        expect(response).to have_http_status(302)
+        expect(response).to admin_sign_in
       end
     end
 
-    describe "GET #new" do
-      it "returns http 302" do
+    describe 'GET #new' do
+      it 'redirect to admin/sign_in' do
         get :new
-        expect(response).to have_http_status(302)
+        expect(response).to admin_sign_in
       end
     end
 
-    describe "GET #edit" do
-      it "returns http 302" do
+    describe 'GET #edit' do
+      it 'redirect to admin/sign_in' do
         get :edit, { id: symptom.id}
-        expect(response).to have_http_status(302)
+        expect(response).to admin_sign_in
       end
     end
   end
