3108a53ceb79dda324491f0ffdbfe8f6331b8ec7
Anton <anton@ondoc.me>
2017-09-02 16:45:17 +0300

    Write tests to PerformedAction model

--- spec/models/performed_action_spec.rb
+++ spec/models/performed_action_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PerformedAction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :actor }
+  it { is_expected.to belong_to :subject }
 end
