ac1364fec4422f02279123a7b708fc623cb0ac69
Anton <anton@ondoc.me>
2017-12-15 00:10:45 +0300

    Update module Recordable

--- app/controllers/recordable.rb
+++ app/controllers/recordable.rb
@@ -1,5 +1,32 @@
 module Recordable
   def record(actor, subject, action)
-    PerformedAction.create(actor: actor, subject: subject, action: action)
+    record = Record.new(actor, subject, action)
+    record.send_event
+  end
+
+  class Record
+    def initialize(actor, subject, action)
+      @params = {
+          actor_id: actor.id,
+          actor_type: actor.class,
+          subject_id: subject.id,
+          subject_type: object_type(subject),
+          action: action
+      }
+    end
+
+    def send_event
+      response = RestClient.post(
+          "http://#{ENV['HISTORY_HOST']}:#{ENV['HISTORY_PORT']}/event",
+          @params
+      )
+      send_event if response.code != 200
+    end
+
+    private
+
+    def object_type(subject)
+      subject.decorated? ? Draper.undecorate(subject).class : subject.class
+    end
   end
 end
